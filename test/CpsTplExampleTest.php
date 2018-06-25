b2fad7521e8ea892c1853ffcf44a1e72579d11be
李伟 <liwei1987821@126.com>
2018-06-25 10:33:42 +0800

    update

---
+++ test/CpsTplExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liwei
+ * Date: 2018/6/25
+ * Time: 上午10:30
+ */
+
+class CpsTplExampleTest{
+
+    public function actionIndex(){
+        return true;
+    }
+}
